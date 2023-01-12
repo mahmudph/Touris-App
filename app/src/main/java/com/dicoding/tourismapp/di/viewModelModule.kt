7eb5c99d679f9c4dae0ec6a4c31214a9e5e64073
mahmud <mahmud120398@gmail.com>
2023-01-12 08:17:51 +0700

    feat: add model module di

---
+++ app/src/main/java/com/dicoding/tourismapp/di/viewModelModule.kt
@@ -0,0 +1,24 @@
+/**
+ * Created by Mahmud on 12/01/23.
+ * mahmud120398@gmail.com
+ */
+
+package com.dicoding.tourismapp.di
+
+import com.dicoding.tourismapp.detail.DetailTourismViewModel
+import com.dicoding.tourismapp.favorite.FavoriteViewModel
+import com.dicoding.tourismapp.home.HomeViewModel
+import com.example.core.domain.usecase.TourismInteractor
+import com.example.core.domain.usecase.TourismUseCase
+import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val useCaseModule = module {
+    factory<TourismUseCase> { TourismInteractor(get()) }
+}
+
+val viewModelModule = module {
+    viewModel { HomeViewModel(get()) }
+    viewModel { FavoriteViewModel(get()) }
+    viewModel { DetailTourismViewModel(get()) }
+}
